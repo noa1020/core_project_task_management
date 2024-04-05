f3becefcde57439a7ebdab03d5c2c2c004cbe66c
noa1020 <noa0533181648@gmail.com>
2024-04-05 14:19:16 +0300

    remove Data

--- Interfaces/ITodolistService.cs
+++ Interfaces/ITodolistService.cs
@@ -6,7 +6,6 @@ namespace todoList.Interfaces
     public interface ITodolistService
     {
         List<Models.Task> GetAll(int userID);
-       // int Count ();
         bool Delete(int id);
          bool Update(int id, Models.Task newTask);
         Models.Task? GetById(int id,int userID) ;
